b9a6d1c4037f4403fb3cf9a6f559e264113a85d8
phittetryne <91614322+phittetryne@users.noreply.github.com>
2021-11-02 17:07:40 +0100

    laget start på overlay
    mangler tinted bakgrunn
    må lange indvd overlays for hver rett, men beholde overlay.content som class

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,1 +1,11 @@
 // Pål and Herman
+
+function openOverlay() {
+    document.getElementById("myOverlay").style.height = "80%";
+    // document.getElementById("backOverlay").style.height = "100%";
+}
+
+function closeOverlay() {
+    document.getElementById("myOverlay").style.height = "0%";
+    // document.getElementById("backOverlay").style.height = "0%";
+}
