104c9caecf32085589b6b5ff560e419fa0b9ccc9
phittetryne <91614322+phittetryne@users.noreply.github.com>
2021-11-03 15:52:14 +0100

    tja, småendringer

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,11 +1,15 @@
 // Pål and Herman
 
 function openOverlay() {
-    document.getElementById("myOverlay").style.height = "80%";
+    document.getElementById("myOverlay").style.height = "140%";
+    // document.getElementById("myOverlay").style.shadow = "0px 0px 200px 200px rgba(0, 0, 0, 0.18)";
+    // document.getElementById("myOverlay").style.display = "block";
     // document.getElementById("backOverlay").style.height = "100%";
 }
 
 function closeOverlay() {
     document.getElementById("myOverlay").style.height = "0%";
+    // document.getElementById("myOverlay").style.shadow = "0px 0px 200px 200px rgba(0, 0, 0, 0)";
+    // document.getElementById("myOverlay").style.display = "none";
     // document.getElementById("backOverlay").style.height = "0%";
 }
