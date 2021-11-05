c9ace6fd57f3333ab8168247c68c098ad32c3717
phittetryne <91614322+phittetryne@users.noreply.github.com>
2021-11-05 18:57:37 +0100

    styling overlay
    stylet overlay generelt, mangler indvd for forksjellige retter, samt telleboks og legg i handlekurv

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,15 +1,13 @@
 // Pål and Herman
 
 function openOverlay() {
-    document.getElementById("myOverlay").style.height = "140%";
-    // document.getElementById("myOverlay").style.shadow = "0px 0px 200px 200px rgba(0, 0, 0, 0.18)";
-    // document.getElementById("myOverlay").style.display = "block";
-    // document.getElementById("backOverlay").style.height = "100%";
+    document.getElementById("myOverlay","overlay-back").style.height = "140%";
+    
+   
 }
 
 function closeOverlay() {
-    document.getElementById("myOverlay").style.height = "0%";
-    // document.getElementById("myOverlay").style.shadow = "0px 0px 200px 200px rgba(0, 0, 0, 0)";
-    // document.getElementById("myOverlay").style.display = "none";
-    // document.getElementById("backOverlay").style.height = "0%";
+    document.getElementById("myOverlay","overlay-back").style.height = "0%";
+   
 }
+
