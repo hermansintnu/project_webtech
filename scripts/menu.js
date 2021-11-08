8d800ead86d0870cd43343e21b43319aca0b56f3
hermansintnu <hermansi@stud.ntnu.no>
2021-11-08 16:01:43 +0100

    I

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,15 @@
 // Marius and Ståle
+var minus = document.getElementsByClassName('minus');
+var plus = document.getElementsByClassName('plus')
+
 let data=1
 document.getElementById("root").innerText=data;
-function decrement(){
+
+minus.addEventListener('click', function decrement(){
     data=data-1;
     document.getElementById("root").innerText=data;
-}
+})
+
 function increment(){
     data=data+1;
     document.getElementById("root").innerText=data;
