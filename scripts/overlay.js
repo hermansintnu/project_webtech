dfa192e0a6dfd0517847978cec4ccefbb15630f2
phittetryne <91614322+phittetryne@users.noreply.github.com>
2021-11-08 19:49:43 +0100

    gjort overlay responsive

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,47 +1,35 @@
 // Pål and Herman
 function openOverlay() {
-    document.getElementById("myOverlay").style.display = "block";
-}
+    document.getElementById("myOverlay").style.display = "block";}
 function closeOverlay() {
-    document.getElementById("myOverlay").style.display = "none";
-}
+    document.getElementById("myOverlay").style.display = "none";}
 
 
 function openOverlay2() {
-    document.getElementById("myOverlay2").style.display = "block"; 
-}
+    document.getElementById("myOverlay2").style.display = "block";}
 function closeOverlay2() {
-    document.getElementById("myOverlay2",).style.display = "none";
-}
+    document.getElementById("myOverlay2",).style.display = "none";}
 
 
 function openOverlay3() {
-    document.getElementById("myOverlay3").style.display = "block"; 
-}
+    document.getElementById("myOverlay3").style.display = "block";}
 function closeOverlay3() {
-    document.getElementById("myOverlay3",).style.display = "none";
-}
+    document.getElementById("myOverlay3",).style.display = "none";}
 
 
 function openOverlay4() {
-    document.getElementById("myOverlay4").style.display = "block"; 
-}
+    document.getElementById("myOverlay4").style.display = "block";}
 function closeOverlay4() {
-    document.getElementById("myOverlay4",).style.display = "none";
-}
+    document.getElementById("myOverlay4",).style.display = "none";}
 
 
 function openOverlay5() {
-    document.getElementById("myOverlay5").style.display = "block"; 
-}
+    document.getElementById("myOverlay5").style.display = "block";}
 function closeOverlay5() {
-    document.getElementById("myOverlay5",).style.display = "none";
-}
+    document.getElementById("myOverlay5",).style.display = "none";}
 
 
 function openOverlay6() {
-    document.getElementById("myOverlay6").style.display = "block"; 
-}
+    document.getElementById("myOverlay6").style.display = "block";}
 function closeOverlay6() {
-    document.getElementById("myOverlay6",).style.display = "none";
-}
+    document.getElementById("myOverlay6",).style.display = "none";}
