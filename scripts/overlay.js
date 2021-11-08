b55916671a1a4cfdb88fbe958794c0654ebe6bdf
phittetryne <91614322+phittetryne@users.noreply.github.com>
2021-11-08 13:01:58 +0100

    lagt inn forskjellige overlays

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,14 +1,49 @@
 // Pål and Herman
 
 function openOverlay() {
-    document.getElementById("myOverlay").style.height = "140%";
-    document.getElementById("overlay-back").style.height ="100%"
-    
-   
+    document.getElementById("myOverlay").style.height = "750px"; 
 }
 
 function closeOverlay() {
-    document.getElementById("myOverlay","overlay-back").style.height = "0%";
-   
+    document.getElementById("myOverlay").style.height = "0%";
 }
 
+function openOverlay2() {
+    document.getElementById("myOverlay2").style.height = "750px"; 
+}
+
+function closeOverlay2() {
+    document.getElementById("myOverlay2",).style.height = "0%";
+}
+
+function openOverlay3() {
+    document.getElementById("myOverlay3").style.height = "750px"; 
+}
+
+function closeOverlay3() {
+    document.getElementById("myOverlay3",).style.height = "0%";
+}
+
+function openOverlay4() {
+    document.getElementById("myOverlay4").style.height = "750px"; 
+}
+
+function closeOverlay4() {
+    document.getElementById("myOverlay4",).style.height = "0%";
+}
+
+function openOverlay5() {
+    document.getElementById("myOverlay5").style.height = "750px"; 
+}
+
+function closeOverlay5() {
+    document.getElementById("myOverlay5",).style.height = "0%";
+}
+
+function openOverlay6() {
+    document.getElementById("myOverlay6").style.height = "750px"; 
+}
+
+function closeOverlay6() {
+    document.getElementById("myOverlay6",).style.height = "0%";
+}
