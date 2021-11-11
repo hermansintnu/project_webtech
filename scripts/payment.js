0de3181e2a1d857817b077c3c0eb1249d44ed14e
hermansintnu <hermansi@stud.ntnu.no>
2021-11-11 15:22:24 +0100

    some changes

---
+++ scripts/payment.js
@@ -0,0 +1,68 @@
+var option1 = document.getElementById('gloshaugen')
+var option2 = document.getElementById('annet')
+
+// Sends customer to the right confirmation page
+function changeAction() {
+  if(option1.checked == true ) {
+    document.getElementById('payment-form').action = "menu.html"
+  }
+  else {
+    document.getElementById('payment-form').action = "confirmation.html"
+  }
+}
+
+// Maps for payment
+function chooseLocation() {
+    var d = new Date();
+    if (d.getDay() == 1 &&2) {
+      document.getElementById('choice2').innerHTML = "Tunga"
+      document.getElementById('map3').style.display = "none";
+      document.getElementById('map4').style.display = "none";
+    }
+    else if (d.getDay() == 3) {
+      document.getElementById('choice2').innerHTML = "Kalvskinnet"
+      document.getElementById('map2').style.display = "none";
+      document.getElementById('map4').style.display = "none";
+    }
+    else {
+      document.getElementById('choice2').innerHTML = "Dragvoll"
+      document.getElementById('map2').style.display = "none";
+      document.getElementById('map3').style.display = "none";
+    }
+  }
+
+
+/*
+function showMap1() {
+  if(option1.checked == true) {
+    document.getElementById('map2').style.display = "block";
+    document.getElementById('map2').style.display = "none";
+    document.getElementById('map3').style.display = "none";
+    document.getElementById('map4').style.display = "none";
+  }
+ else {
+ }
+}
+
+function showMap2() {
+  var d = new Date();
+  if ( d.getDay() == 1 & 2 || option2.checked == true &&) {
+    document.getElementById('map1').style.display = "none";
+    document.getElementById('choice2').innerHTML = "Tunga"
+    document.getElementById('map3').style.display = "none";
+    document.getElementById('map4').style.display = "none";
+  }
+  else if(d.getDay() == 3 || option2.checked == true) {
+    document.getElementById('map1').style.display = "none";
+    document.getElementById('choice2').innerHTML = "Kalvskinnet"
+    document.getElementById('map2').style.display = "none";
+    document.getElementById('map4').style.display = "none";
+  }
+  else if (d.getDay() == 4 && 5 || option2.checked == true){
+    document.getElementById('map1').style.display = "none";
+    document.getElementById('choice2').innerHTML = "Dragvoll"
+    document.getElementById('map2').style.display = "none";
+    document.getElementById('map3').style.display = "none";
+  }
+}
+*/
